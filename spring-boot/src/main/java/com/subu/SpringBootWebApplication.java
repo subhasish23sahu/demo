379649b25f7020a0218343f8afc95d7443202251
subhasish <subhasish@beyondwhere.com>
2018-02-10 09:10:47 +0530

    third commit

--- spring-boot/src/main/java/com/subu/SpringBootWebApplication.java
+++ spring-boot/src/main/java/com/subu/SpringBootWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-/*Spring Boot Demo*/
+/*Spring Boot*/
 @SpringBootApplication
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
