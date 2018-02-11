cb3831c46d87e701b243820f39ecb78b924adf9e
subhasish <subhasish@beyondwhere.com>
2018-02-11 21:43:21 +0530

    fifth push

--- spring-boot/src/main/java/com/subu/SpringBootWebApplication.java
+++ spring-boot/src/main/java/com/subu/SpringBootWebApplication.java
@@ -16,7 +16,5 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
-
-
 	
 }
